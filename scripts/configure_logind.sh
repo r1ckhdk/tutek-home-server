b31eb460b53ff083d61aaefd93b644dd5ebe9ab6
r1ckhdk <rick.hokama@gmail.com>
2024-11-21 17:38:53 -0300

    add install docker and configure logind scripts

---
+++ scripts/configure_logind.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Check if script is being run as root
+if [ "$(id -u)" -ne 0 ]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+
+CONFIG_FILE="/etc/systemd/logind.conf"
+
+echo "Configuring logind to ignore closed lid"
+
+
+# Backup logind.conf file
+if [ ! -f "${CONFIG_FILE}.bkp" ]; then
+	cp "${CONFIG_FILE}" "${CONFIG_FILE}.bkp"
+	echo "Original file backup saved in ${CONFIG_FILE}.bkp"
+fi
+
+# Change configurations on HandleLid
+sudo sed -i '/^#\?HandleLidSwitch=/c\HandleLidSwitch=ignore' $CONFIG_FILE && \
+sudo sed -i '/^#\?HandleLidSwitchExternalPower=/c\HandleLidSwitchExternalPower=ignore' $CONFIG_FILE && \
+sudo sed -i '/^#\?HandleLidSwitchDocked=/c\HandleLidSwitchDocked=ignore' $CONFIG_FILE && \
+sudo sed -i '/^#\?LidSwitchIgnoreInhibited=/c\LidSwitchIgnoreInhibited=yes' $CONFIG_FILE && \
+sudo sed -i '/^#\?HandleSuspendKey=/c\HandleSuspendKey=ignore' $CONFIG_FILE && \
+sudo sed -i '/^#\?IdleAction=/c\IdleAction=ignore' $CONFIG_FILE
+
+
+echo "Configuration changes applied on ${CONFIG_FILE}"
+
+echo "Restarting service systemd-logind..."
+sudo systemctl restart systemd-logind
+
+echo "Configuration finished. Now the system ignores lid closing"
